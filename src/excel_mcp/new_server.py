ad864ca43b4a2b38a0107a3e875ba515b988cda0
3446798488 <3446798488@qq.com>
2025-03-20 09:00:51 +0000

    feat: 新增处理能力

--- src/excel_mcp/new_server.py
+++ src/excel_mcp/new_server.py
@@ -1,8 +1,7 @@
 import logging
 import sys
 import os
-from typing import Any, List, Dict
-
+from typing import Any, List, Dict, Optional
 from mcp.server.fastmcp import FastMCP
 import pandas as pd
 from .data_handlers import ExcelHandler, CSVHandler
@@ -65,7 +64,8 @@ def run_code_with_excel(
 @mcp.tool()
 def run_code_with_csv(filepath: str, python_code: str, result_file_path: str):
     """
-    写一个 python 代码，入参为 filepath 获取到的 DataFrame 对象，返回一个 DataFrame, 我会帮你写入文件。
+    写一个数据处理 python 代码，入参为 filepath 获取到的 DataFrame 对象，返回一个 DataFrame, 我会帮你写入文件。
+    注意 print 不会被记录，不能使用 print 打印数据。注意换行。
     Args:
         filepath: csv 文件路径
         python_code: 要执行的Python代码, 格式为 def main(origin_df): 返回 pd.DataFrame
@@ -85,52 +85,94 @@ def get_sheet_names(filepath: str) -> List[str]:
 
 
 @mcp.tool()
-def get_columns_excel(filepath: str, sheet_name: str) -> List[str]:
+def get_columns_excel(filepath: str, sheet_name: str) -> str:
     """Get names of all sheets in Excel file."""
-    return excel_handler.get_columns(filepath, sheet_name)
+    return ", ".join(excel_handler.get_columns(filepath, sheet_name))
 
 
 @mcp.tool()
-def get_columns_csv(filepath: str) -> List[str]:
+def get_columns_csv(filepath: str) -> str:
     """Get names of all columns in CSV file."""
-    return csv_handler.get_columns(filepath)
-
-
-@mcp.tool()
-def inspect_data_in_sheet(
-    filepath: str, sheet_name: str, preview_rows: int = 5, preview_type: str = "head"
-) -> str:
-    """查看Excel数据的基本信息，包括数据预览、统计摘要和数据结构。
-
-    Args:
-        filepath: Excel文件路径
-        sheet_name: 工作表名称
-        preview_rows: 预览行数，默认5行
-        preview_type: 预览类型，'head'查看前几行，'tail'查看后几行
-
-    Returns:
-        包含数据信息的格式化字符串
-    """
-    return excel_handler.inspect_data(
-        filepath, preview_rows, preview_type, sheet_name=sheet_name
-    )
-
+    return ", ".join(csv_handler.get_columns(filepath))
 
 @mcp.tool()
-def inspect_data_in_csv(
-    filepath: str, preview_rows: int = 5, preview_type: str = "head"
-) -> str:
-    """查看CSV数据的基本信息，包括数据预览、统计摘要和数据结构。
+def get_missing_values_info_csv(filepath: str)-> str:
+    """获取缺失值信息
 
     Args:
-        filepath: CSV文件路径
-        preview_rows: 预览行数，默认5行
-        preview_type: 预览类型，'head'查看前几行，'tail'查看后几行
+        
+    Returns:
+        包含缺失值信息的数据框
+    """
+    return csv_handler.get_missing_values_info(csv_handler.read_data(csv_handler.get_file_path(filepath)))
+
+@mcp.tool()
+def get_data_unique_values_csv(
+   filepath: str, columns: Optional[List[str]] = None, max_unique: int = 10
+) -> Dict[str, Any]:
+    """获取指定列的唯一值信息
+
+    Args:
+        columns: 需要查看的列，默认为所有列
+        max_unique: 显示的最大唯一值数量，对于唯一值超过此数的列只显示计数
 
     Returns:
-        包含数据信息的格式化字符串
+        包含唯一值信息的字典
     """
-    return csv_handler.inspect_data(filepath, preview_rows, preview_type)
+    return csv_handler.get_data_unique_values(csv_handler.read_data(csv_handler.get_file_path(filepath)))
+
+@mcp.tool()
+def get_column_correlation_csv(
+    filepath: str, method: str = 'pearson', min_correlation: float = 0.5
+) -> str:
+    """获取列之间的相关性
+
+    Args:
+        df: 数据框
+        method: 相关系数计算方法，'pearson', 'kendall', 或 'spearman'
+        min_correlation: 最小相关系数阈值，只返回绝对值大于此值的相关性
+
+    Returns:
+        相关系数矩阵
+    """
+    return csv_handler.get_data_unique_values(csv_handler.read_data(csv_handler.get_file_path(filepath)))
+
+
+# @mcp.tool()
+# def inspect_data_in_sheet(
+#     filepath: str, sheet_name: str, preview_rows: int = 5, preview_type: str = "head"
+# ) -> str:
+#     """查看Excel数据的基本信息，包括数据预览、统计摘要和数据结构。
+
+#     Args:
+#         filepath: Excel文件路径
+#         sheet_name: 工作表名称
+#         preview_rows: 预览行数，默认5行
+#         preview_type: 预览类型，'head'查看前几行，'tail'查看后几行
+
+#     Returns:
+#         包含数据信息的格式化字符串
+#     """
+#     return excel_handler.inspect_data(
+#         filepath, preview_rows, preview_type, sheet_name=sheet_name
+#     )
+
+
+# @mcp.tool()
+# def inspect_data_in_csv(
+#     filepath: str, preview_rows: int = 5, preview_type: str = "head"
+# ) -> str:
+#     """查看CSV数据的基本信息，包括数据预览、统计摘要和数据结构。
+
+#     Args:
+#         filepath: CSV文件路径
+#         preview_rows: 预览行数，默认5行
+#         preview_type: 预览类型，'head'查看前几行，'tail'查看后几行
+
+#     Returns:
+#         包含数据信息的格式化字符串
+#     """
+#     return csv_handler.inspect_data(filepath, preview_rows, preview_type)
 
 
 async def run_server():
